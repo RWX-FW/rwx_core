40f86afc3a3f09b7764bd89e962dd6f7d37a7bf4
Sleepy Rae <67649181+sleepyexe@users.noreply.github.com>
2024-10-25 16:27:01 +0700

    chore(manifest): add description, version, dependencies

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 
 name 'rwx_core'
+description 'Core resource for rwx framework'
 lua54 'yes'
+version '0.0.1'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -10,11 +12,18 @@ shared_scripts {
     'shared/config.lua'
 }
 
-client_scripts{
+client_scripts {
     'client/main.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua',
+}
+
+dependencies {
+    '/server:7290',
+    '/onesync',
+    'ox_lib',
+    'oxmysql'
 }
