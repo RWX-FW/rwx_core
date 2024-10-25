6c59aae56fad14f99f57d6dd4a8d557f64a3a8e1
AjiSufi <69729592+AjiSufi@users.noreply.github.com>
2024-10-25 22:18:42 +0700

    add config

--- shared/config.lua
+++ shared/config.lua
@@ -1,3 +1,19 @@
-rwx.cfg = {
-    identifier = 'steam' -- steam. license
-}
+local cfg = {}
+
+if not IsDuplicityVersion() then
+    
+else
+    cfg.identifier = 'steam' -- steam, license
+    cfg.identifierTypes = {
+        stateId = {
+            valueFunction = function()
+                return lib.string.random('A.......')
+            end,
+        },
+    }
+    cfg.accounts = {cash = 500, bank = 5000}
+    cfg.hungerRate = 5.0
+    cfg.thirstRate = 5.0
+end
+
+rwx.cfg = cfg
